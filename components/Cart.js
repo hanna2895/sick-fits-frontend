1966f5f68333a9a88e946fbac3a57da1234d446d
Hannah Werman <hanna.w28@gmail.com>
2019-02-26 09:56:00 +0700

    cleaning up render prop mess

--- components/Cart.js
+++ components/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
 import User from './User';
 import CartItem from './CartItem';
 import CartStyles from './styles/CartStyles';
@@ -22,36 +23,42 @@ const TOGGLE_CART_MUTATION = gql`
     }
 `;
 
+// ADOPT ->  useful for cleaning up a render prop mess because it 
+// allows us to expose all the queries and mutations to the component
+// and destructure them rather than nesting them.
+
+// write these components as arrow functions and pass in render to
+// get the proptypes error to disappear from the console.
+
+const Composed = adopt({
+    user: ({ render }) => <User>{render}</User>,
+    toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+    localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+})
+
 const Cart = () => (
-    <User>{({ data: {me }}) => {
-        if(!me) return null;
-        console.log(me)
-        return(
-            <Mutation mutation={TOGGLE_CART_MUTATION}>
-                {toggleCart => (    
-                    <Query query={LOCAL_STATE_QUERY}>
-                        {({ data }) => 
-                            console.log(data) || (
-                            <CartStyles open={data.cartOpen}>
-                                <header>
-                                    <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
-                                    <Supreme>{me.name}'s Cart</Supreme>
-                                    <p>You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
-                                </header>
-                                <ul>
-                                    {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem}/>)}
-                                </ul>
-                                <footer>
-                                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                                    <SickButton>Checkout</SickButton>
-                                </footer>
-                            </CartStyles>
-                        )}
-                    </Query>
-                )}
-            </Mutation>
-        )
-    }}</User>
+    <Composed>
+        {({ user, toggleCart, localState} ) => {
+            const me = user.data.me;
+            if(!me) return null;
+            return(   
+                <CartStyles open={localState.data.cartOpen}>
+                    <header>
+                        <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+                        <Supreme>{me.name}'s Cart</Supreme>
+                        <p>You Have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
+                    </header>
+                    <ul>
+                        {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem}/>)}
+                    </ul>
+                    <footer>
+                        <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+                        <SickButton>Checkout</SickButton>
+                    </footer>
+                </CartStyles>
+            )
+        }}
+    </Composed>
 )
 
 export default Cart;
