05fb2d3515bb6da4b74228b858905db49895ed1e
Hannah Werman <hanna.w28@gmail.com>
2019-06-30 20:49:29 +0700

    testing pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -24,7 +24,7 @@ const Pagination = props => (
                 const pages = Math.ceil(count / perPage)
                 const page = props.page;
                 return (
-                    <PaginationStyles>
+                    <PaginationStyles data-test="pagination">
                         <Head>
                             <title>Sick Fits | Page {page} of {pages}</title>
                         </Head>
@@ -36,7 +36,7 @@ const Pagination = props => (
                         }}>
                             <a className="prev" aria-disabled={page <= 1}>&larr; Prev</a>
                         </Link>
-                        <p>Page {page} of {pages}</p>
+                        <p>Page {page} of <span className="totalPages">{pages}</span></p>
                         <p>{count} items total</p>
                         <Link 
                             prefetch
@@ -44,7 +44,7 @@ const Pagination = props => (
                             pathName: 'items',
                             query: { page: page + 1 }
                         }}>
-                            <a className="prev" aria-disabled={page >= pages}>Next &rarr;</a>
+                            <a className="next" aria-disabled={page >= pages}>Next &rarr;</a>
                         </Link>
                     </PaginationStyles>
                 )
@@ -53,3 +53,4 @@ const Pagination = props => (
 )
 
 export default Pagination;
+export { PAGINATION_QUERY };
